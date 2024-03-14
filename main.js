f39cbee00254092c95424adc99c646f7492997e8
dHurlburt <hurlbu19@msu.edu>
2024-03-14 11:33:20 -0400

    added in code for seeing books

---
+++ main.js
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://evzeezotdchftmtljick.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2emVlem90ZGNoZnRtdGxqaWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MjUxODMsImV4cCI6MjAyNjAwMTE4M30.MlrevzO0M9WUQ95609l0u37pwbegaSczpGW9uEOEY2E'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let { data: Books, error } = await supabase
+  .from('Books')
+  .select('*')
+
+  for (let book of Books){
+    let booklist = document.getElementById('Books');
+    booklist.innerHTML += <li>${book.title}</li>;
+  }
