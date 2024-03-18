45432db2666a222350884bd40ac0e058d9993e13
dHurlburt <hurlbu19@msu.edu>
2024-03-18 12:40:35 -0400

    Got site to finally display data from supabase

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-import { createClient } from '"https://esm.sh/@supabase/supabase-js"'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const supabaseUrl = 'https://evzeezotdchftmtljick.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2emVlem90ZGNoZnRtdGxqaWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MjUxODMsImV4cCI6MjAyNjAwMTE4M30.MlrevzO0M9WUQ95609l0u37pwbegaSczpGW9uEOEY2E'
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -9,9 +9,7 @@ const supabase = createClient(supabaseUrl, supabaseKey)
   .from('Books')
   .select('*')
 
-window.onload = function() {
 for (let Book of Books){
     let Booklist = document.getElementById('Books');
-    Booklist.innerHTML += `<li>${Book.title}</li>`;
+    Booklist.innerHTML += `<tr><td>${Book.title}</td><td>${Book.Author}</td><td>${Book.ISBN}</td></tr>`;
   }
-}
